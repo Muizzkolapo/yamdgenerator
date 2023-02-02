9b56c24c6fc0b95f4c84a64c418f9536988999f8
muizzlateef <mlateef@petsathome.co.uk>
2023-02-02 17:47:27 +0000

    updated
    fixed-some

--- yamdgen/yaml_helpers_function.py
+++ yamdgen/yaml_helpers_function.py
@@ -48,14 +48,6 @@ def check_sql_config(file_path):
     return False
 
 
-def check_file_exists(model_name,path):
-    models_dir = os.path.join(path, "models")
-    for root, dirs, files in os.walk(models_dir):
-        for file in files:
-            if file == f"{model_name}.sql" or file == model_name:
-                return True
-    return False
-
 def find_file(model_name,path):
     models_dir = os.path.join(path, "models")
     for root, dirs, files in os.walk(models_dir):
@@ -68,23 +60,22 @@ def find_file(model_name,path):
 
 def yamlgen(model_name,path, version=2):
     model_name = model_name.split('.', 1)[0]
-    if check_file_exists(model_name,path):
-        model_path = find_file(model_name,path)
-        file_path = find_file_path(model_name,path)
-        mod_name = {"model_names": [model_name]}
-        if not check_sql_config(model_path):
-            cmd = "dbt run-operation generate_model_yaml --args '{}'".format(mod_name)
-            output = subprocess.check_output(cmd, cwd='.', shell=True)
-            output = output.decode("utf-8")
-            ver_details = 'version: {}\n'.format(version)
-            mod_details = output.split('version: 2')[1]
-            file_path = '{}/{}.yml'.format(file_path, model_name)
-            with open(file_path, "w") as text_file:
-                text_file.write(ver_details + mod_details)
-            print("{}.yml has been created in {}".format(model_name, file_path))
-        else:
-            print("{} is ephemeral, skipping...".format(model_name))
-            pass
+    model_path = find_file(model_name,path)
+    file_path = find_file_path(model_name,path)
+    mod_name = {"model_names": [model_name]}
+    if not check_sql_config(model_path):
+        cmd = "dbt run-operation generate_model_yaml --args '{}'".format(mod_name)
+        output = subprocess.check_output(cmd, cwd='.', shell=True)
+        output = output.decode("utf-8")
+        ver_details = 'version: {}\n'.format(version)
+        mod_details = output.split('version: 2')[1]
+        file_path = '{}/{}.yml'.format(file_path, model_name)
+        with open(file_path, "w") as text_file:
+            text_file.write(ver_details + mod_details)
+        print("{}.yml has been created in {}".format(model_name, file_path))
+    else:
+        print("{} is ephemeral, skipping...".format(model_name))
+        pass
 
 
 def generate_yaml_for_models(model_names,path, version=2):
